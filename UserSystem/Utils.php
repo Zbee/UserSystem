c13206879f735acd1a6627a710dd60c97651926d
Ethan Henderson <ethan@zbee.me>
2014-11-29 16:46:08 -0700

    ~currentURL
    Made current URL more accurate.

--- UserSystem/Utils.php
+++ UserSystem/Utils.php
@@ -48,7 +48,15 @@ class Utils {
   * @return string
   */
   public function currentURL () {
-    return "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $pageURL = 'http';
+    if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+      $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+    } else {
+      $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
   }
 
   /**
