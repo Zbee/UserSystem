8029bee06ac0ba2aa8593830378296e3e3c70949
Ethan Henderson <ethan@zbee.me>
2015-06-04 23:15:45 +0000

    Removed unused variables

--- UserSystem/Utils.php
+++ UserSystem/Utils.php
@@ -193,7 +193,7 @@ class Utils {
     $bits = (int) $log + 1;
     $filter = (int) (1 << $bits) - 1;
     do {
-      $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes, $s)));
+      $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
       $rnd = $rnd & $filter;
     } while ($rnd >= $range);
     return $min + $rnd;
